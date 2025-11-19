bb3327a90bae841f259cab88b42106d1528995dd
Bela Varga <100267928+NotBela@users.noreply.github.com>
2025-11-18 22:11:44 -0500

    add models again

---
+++ CompCube-Models/Models/Events/EventScore.cs
@@ -0,0 +1,8 @@
+﻿using CompCube_Models.Models.Match;
+
+namespace CompCube_Models.Models.Events;
+
+public class EventScore(MatchScore score, int placement) : MatchScore(score.User, score.Score)
+{
+    public readonly int Placement = placement;
+}
---
+++ CompCube-Models/Models/Packets/ServerPackets/Event/EventScoresUpdated.cs
@@ -0,0 +1,10 @@
+﻿using CompCube_Models.Models.Events;
+
+namespace CompCube_Models.Models.Packets.ServerPackets.Event;
+
+public class EventScoresUpdated(List<EventScore> scores) : ServerPacket
+{
+    public override ServerPacketTypes PacketType => ServerPacketTypes.EventScoresUpdated;
+
+    public readonly List<EventScore> Scores = scores;
+}
