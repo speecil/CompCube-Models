f641e3cd874f4c393a7d1e4c90835d7192bd985a
Bela Varga <100267928+NotBela@users.noreply.github.com>
2026-01-03 01:47:49 -0500

    added contributors

---
+++ CompCube-Models/Models/Contributors/Contributor.cs
@@ -0,0 +1,8 @@
+﻿namespace CompCube_Server.Models.CompCube_Models.Models.Contributors;
+
+public class Contributor(string name, string role, string profilePictureLink)
+{
+    public readonly string Name = name;
+    public readonly string Role = role;
+    public readonly string ProfilePictureLink = profilePictureLink;
+}
